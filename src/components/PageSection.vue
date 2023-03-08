ea8f22efe52e7f60a3633d6a85214edc0474feb8
ole-kristian-rudjord <s354564@oslomet.no>
2023-03-08 16:47:26 +0100

    Changed basic styling and components

---
+++ src/components/PageSection.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <section>
+    <h1>Ole Kristian Rudjord</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est reprehenderit
+      minus ad nostrum qui recusandae dolor. Ex pariatur velit distinctio sunt
+      tempore cupiditate dicta similique possimus eius! Beatae, accusantium
+      cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Deleniti temporibus debitis maiores voluptates! Voluptates, et quod illum,
+      quidem alias in libero reprehenderit beatae culpa sunt vero, ducimus
+      tempore quaerat delectus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+      dolorem tenetur minus? <a href="#">testlink123.com</a> Ipsam corrupti
+      commodi, voluptatum ducimus temporibus dolorem animi, quis tempore
+      cupiditate officiis consectetur voluptates praesentium ut facere cumque?
+    </p>
+  </section>
+</template>
+
+<style scoped></style>
